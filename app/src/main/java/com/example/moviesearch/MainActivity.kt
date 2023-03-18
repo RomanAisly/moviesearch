afbe3ee42598fe87cd00bc43db1260882acda020
Roman Aisly <romanaisly@gmail.com>
2023-03-18 18:41:03 +0300

    I made homework completely

--- app/src/main/java/com/example/moviesearch/MainActivity.kt
+++ app/src/main/java/com/example/moviesearch/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.moviesearch
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    fun butMenuOn(view: View) {
+        Toast.makeText(this, "Меню", Toast.LENGTH_SHORT).show()
+    }
+    fun butFavoritesOn(view: View) {
+        Toast.makeText(this, "Избраное", Toast.LENGTH_SHORT).show()
+    }
+    fun butWatchLaterOn(view: View) {
+        Toast.makeText(this, "Посмотреть позже", Toast.LENGTH_SHORT).show()
+    }
+    fun butSelectionsOn(view: View) {
+        Toast.makeText(this, "Подборки", Toast.LENGTH_SHORT).show()
+    }
+    fun butSettingsOn(view: View) {
+        Toast.makeText(this, "Настройки", Toast.LENGTH_SHORT).show()
     }
 }
