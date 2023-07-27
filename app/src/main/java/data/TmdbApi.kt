9bc90f301e9152569265580e620555a6a5675657
Roman Aisly <romanaislydevelop@gmail.com>
2023-07-27 20:51:49 +0300

    Now everything works

--- app/src/main/java/data/TmdbApi.kt
+++ app/src/main/java/data/TmdbApi.kt
@@ -3,12 +3,13 @@ package data
 import data.entily.TmdbResultDTO
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface TmdbApi {
     @GET("3/movie/{category}")
     fun getFilms(
-        @Query("category")category: String,
+        @Path("category")category: String,
         @Query("api_key") apiKey: String,
         @Query("language") language: String,
         @Query("page") page: Int
